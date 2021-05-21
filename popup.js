ee259c18eecf8834ce30fd3a985382406eb72064
Nigel Davis <ngeldvis@gmail.com>
2021-05-21 19:35:54 -0400

    added update on toggle

--- popup.js
+++ popup.js
@@ -4,20 +4,21 @@ chrome.storage.sync.get('enabled', ({enabled}) => {
 });
 reportStatus();
 
-toggle.addEventListener('change', () => {
+toggle.addEventListener('change', async () => {
     console.log('checked:' + toggle.checked);
 
     if(toggle.checked) {
         chrome.storage.sync.set({enabled: true});
         reportStatus();
         
-        // let tabs = await chrome.tabs.query({currentWindow: true});
-        // for(var i = 0; i < tabs.length; i++) {
-            //     chrome.scripting.executeScript({
-                //         target: {tabId: tab.id},
-                //         function: removeNotificationCounter
-                //     });
-                // }
+        let tabs = await chrome.tabs.query({currentWindow: true});
+        for(var i = 0; i < tabs.length; i++) {
+            let tab = tabs[i];
+            chrome.scripting.executeScript({
+                target: {tabId: tab.id},
+                function: removeNotificationCounter
+            });
+        }
                 
     } else {
         chrome.storage.sync.set({enabled: false});
@@ -25,15 +26,15 @@ toggle.addEventListener('change', () => {
     }
 });
 
-// // function to remove '(x)' from the beginning of a tab's title
-// function removeNotificationCounter() {
-//     let title = document.title;
-//     if(title[0] === '(' && title.includes(')')) {
-//         let idx = title.indexOf(')');
-//         title = title.slice(idx+1).trim();
-//     }
-//     document.title = title;
-// }
+// function to remove '(x)' from the beginning of a tab's title
+function removeNotificationCounter() {
+    let title = document.title;
+    if(title[0] === '(' && title.includes(')')) {
+        let idx = title.indexOf(')');
+        title = title.slice(idx+1).trim();
+    }
+    document.title = title;
+}
 
 function reportStatus() {
     chrome.storage.sync.get(['enabled'], ({enabled}) => {
